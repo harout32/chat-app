2c844f9997ed6c9584dd4b7ae90a9739e9200c5b
harout <harout.za.artin@gmail.com>
2017-10-29 03:37:13 +0400

    adding timestamps using moment

---
+++ playground/time.js
@@ -0,0 +1,6 @@
+let moment = require('moment');
+
+//jan 1st 1970 00:00:00 am
+let date = moment();
+console.log(date.format('h:mm a'));
+
--- public/js/index.js
+++ public/js/index.js
@@ -10,10 +10,11 @@ socket.on("disconnect", () => {
 });
 
 socket.on('newMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     console.log('new Email arrived');
     console.log(message); 
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from}  ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li);
  });
 
@@ -47,9 +48,10 @@ locationButton.on('click',function(){
 });
 
 socket.on('newLocationMessage',function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from}:  `);
+    li.text(`${message.from}  ${formattedTime}:  `);
     a.attr('href',message.url);
     li.append(a);
     jQuery('#messages').append(li);
