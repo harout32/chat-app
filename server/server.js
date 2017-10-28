107c14e70eb085ba8b82d8291ab06da0e7433b36
harout <harout.za.artin@gmail.com>
2017-10-29 02:36:30 +0400

    adding the send location within the chat

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 
 const port     = process.env.PORT || '3000';
 
-const {generateMessage} = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 let app        = express();
 let server     = http.createServer(app);
@@ -18,24 +18,22 @@ io.on('connection', (socket) => {
     socket.emit('newMessage',generateMessage('Admin','welcome to chat app'))
     socket.broadcast.emit('newMessage',generateMessage('Admin','new user joind'));
 
-    // socket.emit('newMessage', {
-    //     form:'harout@example.com',
-    //     text : 'hey how are you',
-    //     createdAt: new Date().getTime()
-    // });
-
     socket.on('createMessage',(message, callback)=>{
         console.log(JSON.stringify(message,undefined,2));
     //to emit the the new message to all the users
     io.emit('newMessage',generateMessage(message.from,message.text));    
     callback('this is from the server');
     });
+    //on create location 
+    socket.on('createLocationMessage',(location) => {
+        io.emit('newLocationMessage',generateLocationMessage('Admin',location.latitude,location.longitude));
+    })
 
+
+    //on disconnecting
     socket.on("disconnect", ()=>{
         console.log('user was disconnected');
     });
-
-
 });
 
 server.listen(port,()=>{
