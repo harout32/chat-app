f92ea725479eafd05c8d38e31a4d6f54db218aaa
harout <harout.za.artin@gmail.com>
2017-10-29 04:12:36 +0400

    using mustach to render new messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,11 +11,14 @@ socket.on("disconnect", () => {
 
 socket.on('newMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('new Email arrived');
-    console.log(message); 
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}  ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    var template = jQuery("#message-template").html() ;
+    var html = Mustache.render(template, {
+        text:message.text,
+        from:message.from,
+        createdAt:formattedTime
+    });
+
+    jQuery("#messages").append(html)
  });
 
 jQuery('#message-form').on('submit',function(e){
@@ -49,10 +52,18 @@ locationButton.on('click',function(){
 
 socket.on('newLocationMessage',function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from}  ${formattedTime}:  `);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    //using jQuery without using mustache
+//     var li = jQuery('<li></li>');
+//     var a = jQuery('<a target="_blank">My Current Location</a>');
+//     li.text(`${message.from}  ${formattedTime}:  `);
+//     a.attr('href',message.url);
+//     li.append(a);
+//     jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template,{
+        createdAt:formattedTime,
+        from:message.from,
+        url:message.url
+    });
+    jQuery('#messages').append(html);
 })
